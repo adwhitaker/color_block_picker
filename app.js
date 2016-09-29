619157cf02ed69ad31530f46a9da01eeda777de1
Alex Whitaker <whitaker.alexander@gmail.com>
2016-09-29 08:04:58 -0500

    add color randomizer

--- app.js
+++ app.js
@@ -1,48 +1,91 @@
 $(document).ready(function () {
-// array of possible colors
-  var colorArray = ['red', 'yellow', 'green', 'blue', 'purple', 'orange']
-
-//var howManyColors = prompt('How many colors would you like?', '5 possible')
-  var howManyColors = 5;
-
-// a for loop to append the colors to the DOM
-  for (var i = 0; i < howManyColors; i++) {
-    $('.boxes').append('<div class="box" id="' + colorArray[i] + '"></div>');
+// random number function
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
   }
 
-// generates a random number based on how many colors chosen
-  var randomNum = randomNumber(1, howManyColors);
-  console.log(randomNum);
+// array of possible colors
+  var colorArray = ['red', 'yellow', 'green', 'blue', 'purple', 'orange', 'maroon']
+  var arrayOfColors = [];
+  var howManyColors = 0;
+  var success = '';
+  var randomNum = 0;
+  var getRandomColor = '';
+  colorArray2 = [];
+// var
+
+// funtion to create a random array of colors based on the colorArray
+  function randomArray () {
+    for (var i = 0; i < colorArray.length; i++) {
+      colorArray2[i] = colorArray[i];
+    }
+    arrayOfColors = [];
+    howManyColors = 5;
+    var addColor = randomNumber(1, howManyColors) - 1;
+// loop to go through the new array of colors and create another array to be printed on the DOM
+    for (var i = 0; i < howManyColors; i++) {
+      addColor = randomNumber(1, colorArray2.length) - 1;
+      var colorSlice = colorArray2.splice(addColor, 1);
+      if (colorSlice === []) {
+      	colorSlice = arrayOfColors[0];
+      }
+      arrayOfColors.push(colorSlice);
+    }
+  }
+
+// this adds the random color name to the DOM. This is the color to click on
+
+  function randomColor() {
+    randomNum = randomNumber(1, howManyColors);
+    $('#color').text(arrayOfColors[randomNum - 1]);
+    getRandomColor = arrayOfColors[randomNum - 1]
+    console.log('arrayOfColors', getRandomColor);
+  }
+
+// a for loop to append the colors to the DOM
+  function addColorToDOM () {
+    for (var i = 0; i < howManyColors; i++) {
+      $('.boxes').append('<div class="box" id="' + arrayOfColors[i] + '"></div>');
+    }
+  }
+
+  randomArray();
+  randomColor();
+  addColorToDOM();
+  console.log(arrayOfColors);
 
 // this checks if the box that is clicked on is equal to the random number and prints answer to DOM
-  var sucess = '';
-  $('.box').on('click', function () {
+  $('.boxes').on('click', '.box', function () {
     var $clickedBox = $(this);
-    var color = colorArray[randomNum - 1];
-    console.log(color);
-    if ( $(this).is('#' + colorArray[randomNum - 1]) ) {
-      console.log('#' + colorArray[randomNum - 1]);
+    console.log('$clickedBox', $clickedBox);
+    if ( $clickedBox.is('#' + getRandomColor) ) {
       $(this).css('background-color', 'pink');
+      $(this).css('border','limegreen solid 2px');
+      success = 'Congrats!!! Play Again!';
+      $('#answer').text(success);
+// 2 second delay once the square is clicked
+      delayedSuccess()
 
-      delayedSucess()
+      function delayedSuccess() {
+        window.setTimeout(successChange, 2000);
+      }
+      function successChange() {
+        $clickedBox.css('background-color', getRandomColor);
 
-      function delayedSucess() {
-        window.setTimeout(sucessChange, 2000);
       }
-      function sucessChange() {
-        $clickedBox.css('background-color', colorArray[randomNum - 1]);
-        sucess = 'Congrats!!!';
-        $('#answer').text(sucess);
-        randomNum = randomNumber(1, howManyColors);
-        console.log(randomNum);
-      }
+// else statement - try again
     } else {
-      sucess = 'Fail.'
-      $('#answer').text(sucess);
+      success = 'Failure. Try Again.'
+      $('#answer').text(success);
     }
   })
-})
 
-function randomNumber(min, max){
-    return Math.floor(Math.random() * (1 + max - min) + min);
-}
+// new game button listener
+  $('button').on('click', function () {
+    $('.box').remove();
+    randomArray();
+    randomColor();
+    addColorToDOM();
+    $('#answer').text('Did you win?')
+  })
+})
